f7a5c2dba421a26917f68e79157a3db718bcb3cf
raul.bometon <raul.bometon@estudiantat.upc.edu>
2023-03-10 13:38:37 +0000

    tasca #3 finalitzada

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ApplicationRecord
+  validates :author, length: {minimum: 4}
+  validates :content, length: {maximum: 280, minimum: 4}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :tweets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  root 'tweets#index'
 end
